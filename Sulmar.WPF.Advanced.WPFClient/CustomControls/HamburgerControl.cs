aa8a373c7c553ffa33427bab0bf2a271d5c2d88e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-01-17 10:54:17 +0100

    hamburger control

---
+++ Sulmar.WPF.Advanced.WPFClient/CustomControls/HamburgerControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sulmar.WPF.Advanced.WPFClient.CustomControls
+{
+    public class HamburgerControl : ContentControl
+    {
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsExpanded.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(HamburgerControl), new PropertyMetadata(false));
+
+
+    }
+}
